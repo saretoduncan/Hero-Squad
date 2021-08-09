6d85ace70184a2e04b59e52ea99361c374b6bb83
saretoduncan <saretoduncan@gmail.com>
2021-08-09 19:36:54 +0300

    Test and pass all Test on hero class

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -15,6 +15,35 @@ class HeroTest {
         assertEquals("spider powers", hero.getSpecialPower());
         assertEquals("sympathy", hero.getWeakness());
     }
+@Test
+    void ninjaInstance_test_true(){
+    Hero hero= new Hero("spiderman", 25, "spider powers","sympathy");
+    assertEquals("spiderman", Hero.getNinjaInstance().get(0).getName());
+    assertEquals(25, Hero.getNinjaInstance().get(0).getAge());
+    assertEquals("spider powers", Hero.getNinjaInstance().get(0).getSpecialPower());
+    assertEquals("sympathy", Hero.getNinjaInstance().get(0).getWeakness());
+
+}
+@Test
+    void deleteNinja_idTest_Instance_true(){
+    Hero hero= new Hero("spiderman", 25, "spider powers","sympathy");
+    Hero hero1= new Hero("superman",33,"flying", "water");
+
+    Hero.getNinjaInstance().add(hero);
+    Hero.getNinjaInstance().add(hero1);
+    int heroLength= Hero.getNinjaInstance().size();
+    Hero.getNinjaInstance().remove(hero1.getId()-1);
+    assertNotEquals(heroLength,Hero.getNinjaInstance().size());
+
+
+}
+@Test
+    void clearNinjaInstance(){
+    Hero hero= new Hero("spiderman", 25, "spider powers","sympathy");
+    Hero hero1= new Hero("superman",33,"flying", "water");
+    Hero.clearNinjaInstance();
+    assertEquals(0,Hero.getNinjaInstance().size());
+}
 
 
 }
