1418ab93605acc451e3f994fde9db05fb6afef51
saretoduncan <saretoduncan@gmail.com>
2021-08-10 19:17:13 +0300

    add file hero form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,16 @@
-public class App {
-    public static void main(String[] args){
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+    public class App {
+        public static void main(String[] args){
+            staticFileLocation("/public");
+         get("/",(request, response) -> {
+             Map<String, Object> model= new HashMap<>();
+             return new ModelAndView(model,"index.html");
+         }, new HandlebarsTemplateEngine());
+        }
     }
-}
