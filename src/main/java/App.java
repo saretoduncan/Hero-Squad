cf9bb1974c0936a9f7bc790b13628bfc72132d33
saretoduncan <saretoduncan@gmail.com>
2021-08-11 13:56:09 +0300

    add validation heros page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +12,25 @@ import static spark.Spark.*;
             staticFileLocation("/public");
          get("/",(request, response) -> {
              Map<String, Object> model= new HashMap<>();
-             return new ModelAndView(model,"index.html");
+             return new ModelAndView(model,"index.hbs");
          }, new HandlebarsTemplateEngine());
+
+         post("/hero/details",(req, res) -> {
+              Map<String, Object> model= new HashMap<String, Object>();
+              String name=req.queryParams("name");
+              int age=Integer.parseInt(req.queryParams("age"));
+              String power=req.queryParams("powers");
+              String weakness=req.queryParams("weakness");
+              Hero hero= new Hero(name,age,power,weakness);
+              ArrayList<Hero> arr=Hero.getNinjaInstance();
+              req.session().attribute("nam",arr);
+             model.put("nam", req.session().attribute("nam"));
+             return new ModelAndView(model, "success.html");
+         },new HandlebarsTemplateEngine());
+         get("/hero/details",(request, response) -> {
+                Map<String,Object> model= new HashMap<>();
+             model.put("nam", request.session().attribute("nam"));
+                return  new ModelAndView(model,"success.html");
+            }, new HandlebarsTemplateEngine());
         }
     }
